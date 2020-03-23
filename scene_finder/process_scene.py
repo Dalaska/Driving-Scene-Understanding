aa74378cef96b69fd42c48c108ffc09effd8fab5
Darui Zhang <neozdr@gmail.com>
2020-03-23 17:39:23 +0800

    process scene

--- scene_finder/process_scene.py
+++ scene_finder/process_scene.py
@@ -5,60 +5,103 @@ Created on Mon Mar 23 07:33:50 2020
 
 @author: DFTC_ZHANGDR
 """
-dic = {'ACC':{'straight':[],\
-              'curv':[]},\
-        'AEB':{'straight':[],\
-               'curv':[]}}
+import pandas as pd
 
-dic_dat = {'ACC':{'straight':['DR202021','DR202022'],\
-              'curv':['DR202021']},\
-        'AEB':{'straight':['DR202021'],\
-               'curv':['DR202021']}}
 
-a = list(dic.keys())
+class Scene():
+    def __init__(self,video_id,time_start,time_end,tag,description):
+        self.video_id = video_id
+        self.time_start = time_start
+        self.time_end = time_end
+        self.id = hash(str(time_start+time_end))
+        self.tag = tag
+        self.description = description
+        
+    def save(self,file_path):
+        #
+        data ={'id':[self.video_id],'time_start':[self.time_start],\
+               'time_end':[self.time_end],'tag':[self.tag],\
+               'description':description}
+        frame = pd.DataFrame(data)
+        frame.to_csv(file_path, mode='a',index=False,header=False)#不将索引和列名写入数据 # 追加写入
+        
+# show structure
+def show_struct(dic):
+    print('')
+    print('.structure')
+    for key1 in list(dic.keys()):
+        print('├──',key1)
+        for key2 in list(dic[key1].keys()):
+            print('│  ',' └──',key2)
  
-# show content
-print('.')
-for key1 in list(dic.keys()):
-    print('├──',key1)
-    for key2 in list(dic[key1].keys()):
-        print('│  ',' └──',key2)
-    
 # get test content
-tags = ['AEB','curv','ni_guang','cam_jump']  
-scene_id = 'dz2020'
-
-for key1 in list(dic.keys()):
-    for key2 in list(dic[key1].keys()):
-        tag_needed = [key1,key2]
-        if set(tag_needed) <= set(tags): # has all requiredd tags
-          is_select = True
-          dic[key1][key2].append(scene_id)
+def group_testcase(dic,scene):
+    for key1 in list(dic.keys()):
+        for key2 in list(dic[key1].keys()):
+            tag_needed = [key1,key2]
+            if set(tag_needed) <= set(scene.tag): # has all requiredd tags
+                dic[key1][key2].append(scene)
+    return(dic)
           
 # show test dictionary
-print('.')
-for key1 in list(dic.keys()):
-    print('├──',key1)
-    for key2 in list(dic[key1].keys()):
-        print('│  ',' └──',key2)
-        for elem3 in dic[key1][key2]:
-            print('│       ',' └──',elem3,tags)
-            
+def show_testcase(dic):
+    print('.test_data')
+    for key1 in list(dic.keys()):
+        print('├──',key1)
+        for key2 in list(dic[key1].keys()):
+            print('│  ',' └──',key2)
+            for scene3 in dic[key1][key2]:
+                print('│       ',' └──',scene3.video_id,\
+                      scene3.time_start,'-',scene3.time_end,scene3.tag)
+                         
 # get test list
-test_list = [] 
-for key1 in list(dic.keys()):
-    for key2 in list(dic[key1].keys()):
-        for elem3 in dic[key1][key2]:
-            test_list.append(elem3)
+def get_testlist(dic):
+    test_list = [] 
+    test_videoid = []
+    for key1 in list(dic.keys()):
+        for key2 in list(dic[key1].keys()):
+            for elem3 in dic[key1][key2]:
+                test_list.append(elem3)
+                test_videoid.append(elem3.video_id)
+    return(test_list,test_videoid)    
+
+# find test case
+def find_testcase(dic,data):
+    for row in data.iterrows():
+        video_id = row[1][ 'video_id']
+        time_start = row[1][ 'time_start']
+        time_end = row[1][ 'time_end']
+        tag = eval(row[1][ 'tag'])# 输出每一行
+        description = ''
+        scene = Scene(video_id, time_start, time_end,\
+                      tag,description)    
+        dic_dat = group_testcase(dic,scene)
+    return(dic_dat)
+
+#===========================================================================
+# test example
+dic = {'ACC':{'straight':[],\
+              '弯道':[]},\
+        'AEB':{'弯道':[],\
+               'curv':[]}}
             
-print(test_list)            
-# tetst prints
-'''
-print('.')
-print('├──','ACC')
-print('│  ',' └──','str')
-print('│          ',' └──','data1')
-print('│  ',' └──','curv')
-print('├──','LKA')
-print('│  ',' └──','str')
-'''
+# show structure
+show_struct(dic)                        
+# read csv
+
+data = pd.read_csv("log.csv") 
+
+# show data
+data.head()
+
+# find test case 
+dic_dat = find_testcase(dic,data)
+
+show_testcase(dic_dat)  
+
+test_list,test_videoid = get_testlist(dic_dat) 
+
+# save test list
+df_testvideo = pd.DataFrame(test_videoid) 
+file_path = 'test_list.csv'
+df_testvideo.to_csv(file_path,index=False,header=False)
--- scene_finder/scne_finder.py
+++ scene_finder/scne_finder.py
@@ -3,23 +3,27 @@ import pandas as pd
 import csv
 
 class Scene():
-    def __init__(self,video_id,time_start,time_end,tag):
+    def __init__(self,video_id,time_start,time_end,tags,description):
         self.video_id = video_id
         self.time_start = time_start
         self.time_end = time_end
         self.id = hash(str(time_start+time_end))
         self.tags = tags
+        self.description = description
     def save(self,file_path):
-        data ={'id':[self.video_id],'time_start':[self.time_start],'time_end':[self.time_end],'tags':[self.tags]}
+        data ={'id':[self.video_id],'time_start':[self.time_start],\
+               'time_end':[self.time_end],'tags':[self.tags],\
+               'description':description}
         frame = pd.DataFrame(data)
-        frame.to_csv(file_path,index=False,header=False)#不将索引和列名写入数据
+        frame.to_csv(file_path, mode='a',index=False,header=False)#不将索引和列名写入数据 # 追加写入
 
-video_id = 'DR20110322'
+video_id = 'DR2330322'
 time_start = 11.1
 time_end = 20.5
-tags = ['ni_guang','cam_jump']
+tags = ['ACC','curv',]# 'AEB','straight'
+description = 'good dat'
 
-scene = Scene(video_id,time_start,time_end,tags)
+scene = Scene(video_id,time_start,time_end,tags,description)
 scene.save('log.csv')
 
 pass
